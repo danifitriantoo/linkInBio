01be8848bf538fd42297e74f436d597d40529758
Dani Fitrianto Nugraha <60064236+danifitriantoo@users.noreply.github.com>
2021-11-12 18:32:22 +0700

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function visitDribbble () {
+    window.location='http://dribbble.com/danifitrianto';
+}
+
+function visitTrakteer () {
+    window.location='http://trakteer.id/danifitrianto';
+}
+
+function visitGitLab () {
+    window.location='http://gitlab.com/danifitriantoo';
+}
+
+function visitBehance () {
+    window.location='htt://behance.net/danifitrianto';
+}
